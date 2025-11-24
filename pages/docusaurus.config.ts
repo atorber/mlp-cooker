c083f28caf50b7118129eed0def371fdd0f49f7c
luyuchao <atorber@163.com>
2025-11-24 18:44:36 +0800

    chore: Update site production URL

--- pages/docusaurus.config.ts
+++ pages/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/atorber/mlp-cooker',
+  url: 'https://vlist.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/mlp-cooker/',
